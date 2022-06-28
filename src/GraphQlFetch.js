f2cd9473551a93fae7783cc49795edae4115d74b
José Carlos Marquetti Junior <cinzadiscos@gmail.com>
2022-06-28 14:54:36 -0300

    Add grid system

--- src/GraphQlFetch.js
+++ src/GraphQlFetch.js
@@ -12,7 +12,7 @@ Array.prototype.pushIfNotIncluded = function (element) {
 }
 
 export function get_gauge_addr() {
-  return [
+  return `[
     "0xea84fb3f7df57e8d94e1caaee20541f5d0b3a59072c2c139dae69c55e5fc7781",
     "0x2970eee6aa5406d4bac0c0a1a3d8b18f34c736160606dd62e09234ebffbda5ad",
     "0x80776bc36dfe3f5ea1b4c28f3328c75863815edf7ec35dd1cc8e434d5e94306e",
@@ -25,7 +25,7 @@ export function get_gauge_addr() {
     "0x64461eed3df8ec4339975c0ad4d80820ba454756e30f48e575baa0905c4f6de6",
     "0xae009d3fc6517df8d2761a891be63a8a459e68e54d0b8043de176070a23ac51c",
     "0xc7f724eb3473316aef7d0fa7c81d3a50614760cd82ada0c1a08eab6c16e53fda"
-  ];
+  ]`;
 }
 
 const font_color = "#FFFFFF";
@@ -33,6 +33,7 @@ const font_family = '"Console","Fixedsys",monospace';
 
 export function get_chart_options() {
   return {
+    responsive: true,
     maintainAspectRatio: false,
     color: font_color,
     interaction: {
