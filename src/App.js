d86e9e976a6ef98d2daef364ee07dd9ca11b3888
José Carlos Marquetti Junior <cinzadiscos@gmail.com>
2022-07-01 09:10:50 -0300

    basic layout

--- src/App.js
+++ src/App.js
@@ -4,33 +4,41 @@ import './App.css';
 import { Container, Row, Col } from "react-grid-system";
 import AllGaugeChart from './AllGaugeChart';
 import TotalVotesChart from './TotalVotesChart';
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route, NavLink } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <HashRouter>
         <Container>
+          <header className="App-header">
+            <Row align="center" justify="end">
+
+              <Col>
+
+                <h1 className="logo">Qi Wars Qannel</h1>
+                <small><span className="table">&#x253B;&#x2501;&#x253B;</span> &#xFE35; <span className="qimp">&#x10DA;(QwQ&#x10DA;)</span></small>
+
+              </Col>
+              <Col style={{ textAlign: "end" }}>
+                <NavLink to="/">All Gauges</NavLink>
+                <NavLink to="/qipowah">Qipowah</NavLink>
+              </Col>
+            </Row>
+          </header>
           <Row>
             <Col>
-              <img src={logo} className="App-logo" alt="logo" />
-              <p><span className="table">&#x253B;&#x2501;&#x253B;</span> &#xFE35; <span className="qimp">&#x10DA;(QwQ&#x10DA;)</span></p>
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <HashRouter>
-                <Routes>
-                  <Route path="/" element={<AllGaugeChart />} />
-                  <Route path="/qipowah" element={<TotalVotesChart />} />
-                </Routes>
-              </HashRouter>
+              <Routes>
+                <Route path="/" element={<AllGaugeChart />} />
+                <Route path="/qipowah" element={<TotalVotesChart />} />
+              </Routes>
+
             </Col>
           </Row>
         </Container>
 
-      </header>
-    </div>
+      </HashRouter>
+    </div >
   );
 }
 
