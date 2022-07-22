ea0ba5e4a3f89cded9d515ef11eecab8b2d6243c
iwasthesword <cinzadiscos@gmail.com>
2022-07-22 11:27:05 -0300

    feat: object reverser

--- src/QwQ.js
+++ src/QwQ.js
@@ -355,4 +355,5 @@ export class VAULTS {
         "Fantom": this.FANTOM,
         "Avalanche": this.AVALANCHE,
     }
+    static invert = obj => Object.fromEntries(Object.entries(obj).map(a => a.reverse()))
 }
