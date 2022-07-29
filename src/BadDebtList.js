c475a2ce2cde229d51645839200f7404c87eebde
iwasthesword <cinzadiscos@gmail.com>
2022-07-29 09:14:14 -0300

    fix: bad debt for collateral that actually works

--- src/BadDebtList.js
+++ src/BadDebtList.js
@@ -3,7 +3,7 @@ import BadDebt from "./BadDebt";
 export default function BadDebtList() {
     return (
         <>
-            <BadDebt address="0x3fd939b017b31eaadf9ae50c7ff7fa5c0661d47c" />
+            <BadDebt address="0x7cbf49e4214c7200af986bc4aacf7bc79dd9c19a" />
         </>
     );
 }
