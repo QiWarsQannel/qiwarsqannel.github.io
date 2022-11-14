4846b2c578c23eca150b5acadfb7e3cf4f9cb29a
iwasthesword <cinzadiscos@gmail.com>
2022-11-14 09:59:44 -0300

    Add exclude list addresses

--- src/QiBuyBacks.js
+++ src/QiBuyBacks.js
@@ -10,14 +10,19 @@ export default function QiBuyBacks(props) {
         "0x1d8a6b7941ef1349c1b5e378783cd56b001ecfbc",
         "0xf0f5f7c21d181b7a1f9aa36ed46db3e620eda385"
     ];
+    const not_buyback_wallets = [
+        "0xa5e0829caced8ffdd4de3c43696c57f7d7a678ff", // Quickswap router
+        "0xffd2aa58cca3a44120aaa42cea2852348a9c2ea6", // Masterchef QI-MATIC + MAI-USDC
+        "0x574fe4e8120c4da1741b5fd45584de7a5b521f0f", // Old Masterchef QI-MATIC + MAI-USDC
+    ]
     const fresh_threshold = 15;
     const fresh_color = "6ce24c";
     const { data } = useMoralisQuery(
         "PolygonTokenTransfers", 
         (query) => query
         .containedIn("to_address", dao_wallets)
+        .notContainedIn("from_address",not_buyback_wallets)
         .equalTo("token_address", "0x580a84c73811e1839f75d86d75d88cca0c241ff4")
-        .notEqualTo("from_address","0x574fe4e8120c4da1741b5fd45584de7a5b521f0f")
         .limit(500)
         );
     const getData = data => data;
